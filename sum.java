d64c55a673bbdaf16bcba2e95c5f8a1b749bd4c1
vg412 <47376554+vg412@users.noreply.github.com>
2019-02-06 02:49:34 -0800

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num = 50, i = 1, sum = 0;
+
+        while(i <= num)
+        {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
+	
